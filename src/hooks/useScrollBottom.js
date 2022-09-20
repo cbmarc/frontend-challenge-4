43203ff8eb768cecb69fb929bcf5b4cecfdab24f
Marc Carné Bori <marc.carne@werlabs.com>
2022-09-20 22:22:53 +0200

    Document useScrollBottom hook

--- src/hooks/useScrollBottom.js
+++ src/hooks/useScrollBottom.js
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 
+/**
+ * Hook used to scroll bottom of a container described by elementId.
+ * @param {string} elementId of the container on which to scroll bottom.
+ * @param {Array} dependencyArray used to determine when to run the scroll bottow.
+ */
 export const useScrollBottom = (elementId, dependencyArray) => {
   useEffect(() => {
     const container = document.getElementById(elementId);
@@ -7,6 +12,10 @@ export const useScrollBottom = (elementId, dependencyArray) => {
   }, [dependencyArray, elementId]);
 };
 
+/**
+ * Scrolls to the bottom of the provided container.
+ * @param {HTMLElement | null} container container in which to scroll bottom.
+ */
 function scrollBottom(container) {
   if (container) {
     container.scrollTo({
