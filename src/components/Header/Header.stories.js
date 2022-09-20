30b47511da4a54144ecc9b01842ae89ac20eb548
Marc Carné Bori <marc.carne@werlabs.com>
2022-09-20 16:12:48 +0200

    Header story.

---
+++ src/components/Header/Header.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from './Header';
+
+const story = {
+  title: 'Header',
+  component: Header,
+};
+export default story;
+
+export const Default = () => <Header>I'm a header</Header>;
